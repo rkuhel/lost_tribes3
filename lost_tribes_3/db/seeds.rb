e27e54aad48ca1fa0d20832b32a0eda1bb04b746
Ryan Kuhel <ryan.kuhel@gmail.com>
2013-07-30 18:32:59 -0400

    seeds file

--- lost_tribes_3/db/seeds.rb
+++ lost_tribes_3/db/seeds.rb
@@ -1,7 +1,20 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Beer.delete_all
+
+#..... 
+
+
+Beer.create(
+	title: 'Shikra Ale', 
+	description: %{Shikra's bold and refreshing hop character is elegantly balanced by a special blend of Caramel Malts.},
+	image_url: 'http://brewtribe.herokuapp.com/PintCutoutShikra.png',
+	price: 12.99)
+Beer.create(
+	title: 'Tej', 
+	description: %{Our ancient style brew, described as "the drink of kings," is fermented with all natural wildflower honey and spicy gesho herbs.},
+	image_url: 'http://brewtribe.herokuapp.com/LostTribes-tejbottle.png',
+	price: 15.99)
+Beer.create(
+	title: 'Light', 
+	description: %{Lost Tribes’ Light is cold brewed and cold aged to perfection in a unique brewing process.},
+	image_url: 'http://brewtribe.herokuapp.com/beerPintlightcut.png',
+	price: 13.99)
