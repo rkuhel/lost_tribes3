a7f7ebf84101d6ea3ab653aaaad72516ab5a43c6
Michael Reinhart <mreinhart88@gmail.com>
2013-07-30 20:51:24 -0400

    made user omni-auth-able

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
-  # :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  # :lockable, and :timeoutable
+  devise :database_authenticatable, :registerable, :omniauthable,
          :recoverable, :rememberable, :trackable, :validatable
 end
