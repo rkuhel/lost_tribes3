51d742f363130ddffa9239d6f8dca787420c3c8e
Ryan Kuhel <ryan.kuhel@gmail.com>
2013-07-30 20:46:29 -0400

    events CRUD

---
+++ lost_tribes_3/app/views/events/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @event, :title, :description, :image_url, :street, :street2, :city, :state, :zip, :start_date, :start_time, :end_date, :end_time, :creator_id, :price, :created_at, :updated_at
--- lost_tribes_3/db/schema.rb
+++ lost_tribes_3/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130730214537) do
+ActiveRecord::Schema.define(version: 20130731002822) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,25 @@ ActiveRecord::Schema.define(version: 20130730214537) do
     t.text     "description"
     t.string   "image_url"
     t.decimal  "price"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "events", force: true do |t|
+    t.string   "title"
+    t.text     "description"
+    t.decimal  "price"
+    t.string   "image_url"
+    t.string   "street"
+    t.string   "street2"
+    t.string   "city"
+    t.string   "state"
+    t.string   "zip"
+    t.date     "start_date"
+    t.time     "start_time"
+    t.date     "end_date"
+    t.time     "end_time"
+    t.integer  "creator_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
