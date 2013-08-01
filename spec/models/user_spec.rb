a744063e3639148622524bf1d14b5804b639106d
Michael Reinhart <mreinhart88@gmail.com>
2013-08-01 15:37:37 -0400

    tested our way to the point where we need can can. single user validation for name test passes

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,18 +1,20 @@
 require 'spec_helper'
+require "cancan/matchers"
+
 
 describe User do
  subject(:user) {create(:user)}
   describe 'validations' do
-    it {should validate_presence_of(:zip_code)}
+    it {should validate_presence_of(:name)}
   end
 
- #  context "Vendors" do 
- #    it "requires vendors to have street address" do
- #      vendor = create(:vendor)
- #      expect(vendor).to be_valid
- #      vendor.street_address = nil 
- #      expect(vendor).to be_invalid
- #    end
+  context "Vendors" do 
+    it "requires vendors to have street address" do
+      vendor = create(:vendor)
+      expect(vendor).to be_valid
+      vendor.street_address1 = nil 
+      expect(vendor).to be_invalid
+    end
  #    it "vendors should have city" do 
  #      vendor = create(:vendor)
  #      vendor.city = nil
@@ -28,7 +30,7 @@ describe User do
  #      vendor.phone_number = nil
  #      expect(vendor).to be_invalid
  #    end
- #  end
+  end
 
  #  context "Users" do 
  #    it "users do not require street address" do
