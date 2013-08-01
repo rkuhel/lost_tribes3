3628d9409aa860249222bb8b99cd4515e949d464
Chris Eichler <chriseichler@gmail.com>
2013-08-01 16:11:22 -0400

    set up much of cancan with admin user role

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
+
+  attr_accessible :roles
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
@@ -6,4 +8,11 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   validates_presence_of :name
+
+  ROLES = %w[admin vendor customer]
+
+  # def roles=(roles)
+  #   self.roles_mask
+
+  # end
 end
