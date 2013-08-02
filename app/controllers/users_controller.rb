9eeb4f37fe8f9879d45f9e10b2ed49114ece4ac5
Michael Reinhart <mreinhart88@gmail.com>
2013-08-02 14:47:57 -0400

    made the users controller. added users as a resource in the routes.rb file. starting to build out the user pages. i also added some buttons to the nav bar. Ultimately i want to have users be able to see the items in their cart. starting small for now tho, just building out the user views.

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,24 @@
+class UsersController < ApplicationController
+
+  def index
+  end
+
+  # def new
+  # end
+
+  # def create
+  # end
+
+  # def show
+  # end
+
+  # def edit
+  # end
+
+  # def create
+  # end
+
+  # def destroy
+  # end
+
+end
