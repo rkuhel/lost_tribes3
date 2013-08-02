895b067cb2b0262e8a178313b7a5e10744fa52f0
Michael Reinhart <mreinhart88@gmail.com>
2013-08-01 20:00:32 -0400

    fixed merge conflict issues

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,25 +11,14 @@ class User < ActiveRecord::Base
 
   
   validates_presence_of :name
-  
-<<<<<<< HEAD
-  before_create :validate_vendor #if self.role == 'vendor
-  ROLES = %w[admin vendor customer]
 
-  # def roles=(roles)
-  #   self.roles_mask
-
-  # end
-=======
   before_create :validate_vendor
->>>>>>> 304fbd4cd401a1d86302a223520e391e807f4e66
 
   private
 
   def validate_vendor
     if self.role == 'vendor'
       return ( !street_address1.blank? && !city.blank? && !state.blank? && !phone.blank? && !zip_code.blank? )
-      # return !city.blank?
     end
     true
   end
