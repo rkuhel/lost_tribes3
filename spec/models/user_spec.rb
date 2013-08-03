69f69a09a7d13ad78c2c0b26b87772b9b7bd633b
Michael Reinhart <mreinhart88@gmail.com>
2013-08-03 18:52:25 -0400

    tests are not good as currently written. decided to table that work and begin on views.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -16,12 +16,18 @@ describe User do
   end
 
   context 'customer role' do
-    it 'has the role of customer'
-    it 'does not require street address'
-    it 'does not require a city'
-    it 'does not require a state'
-    it 'does not require a phone number'
-    it 'does not require a zip code'
+    before do
+      user.role = 'customer'
+    end
+
+    it 'has the role of customer' do
+      user.save.should be_true
+      user.role.should eq 'customer'
+    end
+    it 'can be created without the parameters required for a vendor' do
+      user.save.should be_true
+    end
+
     it 'cannot create an event'
     it 'cannot edit an event'
     it 'cannot delete an event'
@@ -29,7 +35,13 @@ describe User do
   end
 
   context 'vendor role' do
-    it 'has the role of vendor'
+    before do
+      user.role = 'vendor'
+    end
+    it 'has the role of vendor' do
+      # user.save.should be_true
+      user.role.should eq 'vendor'
+    end
     it "Can't be created without required parameters" do
       user.role = 'vendor'
       user.save.should be_false
@@ -44,17 +56,20 @@ describe User do
       user.zip_code = '22222'
       user.save.should be_true
     end
+      
+    # it{should be_able_to(:manage, Event.new(:creator => user)) }
 
-    it 'can create an event'
-    it 'can edit an event it created'
     it 'cannot edit an event it did not create'
     it 'can delete an event it created'
     it 'cannot delete an event it did not create'
   end
 
   context 'admin role' do
-    it 'has the role of admin' do
+    before do
       user.role = 'admin'
+    end
+    it 'has the role of admin' do
+      user.save.should be_true
       user.role.should eq 'admin'
     end
 
@@ -70,5 +85,37 @@ describe User do
     it { should have_and_belong_to_many(:events) }
     it 'has many carts'
   end
+
+  describe 'abilities' do
+    ability = Ability.new(user)
+    ability.should be_able_to(:manage, Event.new)
+
+
+    # subject {ability}
+    # let(:ability){ Ability.new(user) }
+    # let(:user){ nil }
+
+    # context 'when it is an admin' do
+    #   let(:admin){ Factory(:admin) }
+
+    #   it{ should be_able_to(:manage, Event.new) }
+    # end
+  end
 end
 
+
+
+
+
+
+    # describe 'vendor event management' do
+    #   let(:event) { build(:event1) }
+    #   it 'can create an event' do
+    #     lambda {
+    #       user.created_events << event
+    #     }.should change{user.created_events.length}.from(0).to(1)
+    #     event.creator.should eq user
+    #   end
+    #   it 'can edit an event it created' do
+
+    #   end
