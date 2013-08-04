cdc2a8bad6956d1521f95cf23e1270407be599c3
Michael Reinhart <mreinhart88@gmail.com>
2013-08-04 14:53:31 -0400

    On the user#index page, the edit buttons now link to the edit page for that user, not the current_user (as it was doing in the past). there is nothing on the user#edit page yet, though. thats because we have been using the devise edit page. i will likely copy the devise edit page to the user#edit page.

---
+++ app/assets/javascripts/users.js
@@ -0,0 +1,17 @@
+
+
+function goto_edit_user_page(e){
+  e.preventDefault();
+  // console.log('got into js function!');
+
+  var user_row = $(this).parent().parent();
+  var user_id = user_row.attr('data-user-id');
+
+  location.href="/users/"+user_id+"/edit"
+}
+
+
+
+$(function(){
+  $('tbody').on('click', '.edit-user-button', goto_edit_user_page);
+});
