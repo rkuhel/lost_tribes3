01108bf821c08d03e8e79eed49ebf46eef744ec4
Michael Reinhart <mreinhart88@gmail.com>
2013-08-07 00:39:19 -0400

    events in the cart are properly updating quantity if you try and add an event to the cart that already is there. not sure that checkout is working though. it appears to get half way through the method, then it takes a poop. the cart is set to false and a new cart is made, but an error message comes up on the screen. could be local issue just to my machine? idk - has to do with ssl certificates or something. PS - if you delete an event from you user#show page on the list of events you are attending, it doesnt delete your charge. i also doubt that a user destroying the event would destroy its dependent line items. shit gets complicated, huh?

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -33,4 +33,14 @@ class Cart < ActiveRecord::Base
     end
     current_item
   end
+
+  # def add_event(event_id)
+  #   current_item = line_items.find_by_event_id(event_id)
+  #   if current_item
+  #     current_item.quantity += 1
+  #   else
+  #     current_item = line_items.build(event_id: event_id)
+  #   end
+  #   current_item
+  # end
 end
