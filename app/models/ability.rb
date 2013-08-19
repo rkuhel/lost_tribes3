aa3f3b12c7084141f99ded288f6a0afea781afe8
Michael Reinhart <mreinhart88@gmail.com>
2013-08-19 18:31:55 -0400

    cleaned up abilities file. did a lot of work on formatting an event show page. Times of events now have much better displays. Changed some of the old BACK links so they make more sense

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -6,24 +6,18 @@ class Ability
 
     if user.role == 'admin'
       can :manage, :all
+
     elsif user.role == 'vendor'
       can :read, [Beer, Event]
+
       can :create, Event
       can :update, Event do |event|
         event && event.creator_id == user.id
       end
-      # can :edit, Event do |event|
-      #   event && event.creator_id == user.id
-      # end
-      can :show, Cart do |cart|
-        cart && cart.user_id == user.id
+      can :destroy, Event do |event|
+        event && event.creator == user
       end
-      # can :create, LineItem do |li|
-      #   li && li.cart_id == user.current_cart.id
-      # end
-      # can :update, LineItem do |li|
-      #   li && li.cart_id == user.current_cart.id
-      # end
+
       can :update, User do |person|
         person && person.id == user.id
       end
@@ -33,12 +27,11 @@ class Ability
       can :destroy, User do |person|
         person && person.id == user.id
       end
-      can :destroy, Event do |event|
-        event && event.creator == user
+
+      can :show, Cart do |cart|
+        cart && cart.user_id == user.id
       end
-      # can :update, Event do |event|
-      #   event && event.users.include?(user)
-      # end
+
     elsif user.role == 'customer'
       can :read, [Beer, Event]
       can :update, User do |person|
@@ -53,46 +46,9 @@ class Ability
       can :destroy, User do |person|
         person && person.id == user.id
       end
+
     else
       can :read, [Beer, Event]
     end
   end
 end
-
-
-  # def initialize(user)
-  #   # @user ||= User.new # guest user (not logged in)
-  #   # @user.role.each { |role| send(role) }
-
-  #   can :update, User
-
-  #   @user = user || User.new(role: 'guest')
-
-  #   alias_action :create, :read, :update, :destroy, :to => :crud
-
-  #   # if @user.role == nil
-  #   # can :read, :all
-  #   # end
-
-  #   if @user.role == 'admin'
-  #     can :manage, :all
-  #     # can :create, Event
-  #   end
-
-  #   if @user.role == 'vendor'
-  #     can :crud, User, :user_id => @user.id
-  #     can [:read, :create], Event
-  #     can :crud, Event, @user.events.include?(@event)
-  #     # can :manage, Cart, in @user.carts?
-  #   end
-
-  #   if @user.role == 'customer'
-  #     can :crud, User, :user_id => @user.id
-  #     can :read, Event
-  #     # can :manage, Cart, in @user.carts?
-  #     # can :manage, :all
-  #   end
-
-  #   if @user.role == 'guest'
-  #   end
-  # end
